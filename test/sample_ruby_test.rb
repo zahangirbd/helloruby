24139765068052c02658c8490892d4a2152a66ad
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-24 23:47:21 +0100

    just added simple number class to perform some unit testing using rake

--- test/sample_ruby_test.rb
+++ test/sample_ruby_test.rb
@@ -1,10 +1,17 @@
-require './lib/sample_ruby'
 require "test/unit"
+require './lib/sample_ruby'
+require './lib/simple_number'
+
 
 class SampleRubyTest < Test::Unit::TestCase
   def test_it_works
     obj = SampleRuby.new
     puts "testing SampleRuby instance"
     assert obj
+  end
+
+  def test_numbers
+    assert_equal(4, SimpleNumber.new(2).add(2) )
+    assert_equal(6, SimpleNumber.new(2).multiply(3) )
   end
 end
